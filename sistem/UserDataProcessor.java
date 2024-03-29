620cf395044e1f4bdd3edf3b8f489ea2997995e1
NiKiTa <hukuta199701@gmail.com>
2024-03-29 06:54:52 +0300

    Var 3 ready

--- sistem/UserDataProcessor.java
+++ sistem/UserDataProcessor.java
@@ -1,39 +1,44 @@
 package sistem;
 
 import java.util.Scanner;
-import java.io.BufferedWriter;
-import java.io.FileWriter;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.util.regex.Pattern;
 
 public class UserDataProcessor {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите данные в следующем формате, разделенные пробелом: \n" +
-                "Фамилия Имя Отчество дата_рождения номер_телефона пол\n" +
-                "Пример: Иванов Иван Иванович 01.01.1990 1234567890 m\n" +
-                "Требования к данным:\n" +
-                "- Фамилия, Имя, Отчество - строки без цифр и специальных символов.\n" +
-                "- Дата рождения - в формате dd.mm.yyyy (например, 31.12.1990).\n" +
-                "- Номер телефона - целое число без пробелов и специальных символов.\n" +
-                "- Пол - один символ: 'f' (женский) или 'm' (мужской).");
-        String input = scanner.nextLine();
-        processInput(input);
+        while (true) {
+            try {
+                System.out.println("Введите данные в следующем формате, разделенные пробелом: \n" +
+                        "Фамилия Имя Отчество дата_рождения номер_телефона пол\n" +
+                        "Требования к данным:\n" +
+                        "- Фамилия, Имя, Отчество - строки без цифр и специальных символов.\n" +
+                        "- Дата рождения - в формате dd.mm.yyyy (например, 31.12.1990).\n" +
+                        "- Номер телефона - целое число без пробелов и специальных символов.\n" +
+                        "- Пол - один символ: 'f' (женский) или 'm' (мужской).\n" +
+                        "Пример: Иванов Иван Иванович 01.01.1990 1234567890 m");
+                String input = scanner.nextLine();
+                processInput(input);
+                break; // Выход из цикла после успешного ввода данных
+            } catch (Exception e) {
+                System.out.println("-----------------------------------");
+                System.out.println("Ошибка: " + e.getMessage() + " Попробуйте еще раз.");
+                System.out.println("-----------------------------------");
+                // Цикл продолжится, пока данные не будут введены верно
+            }
+        }
     }
 
-    private static void processInput(String input) {
+    private static void processInput(String input) throws IOException {
         String[] parts = input.split(" ");
         if (parts.length != 6) {
-            System.out.println(
+            throw new IllegalArgumentException(
                     "Вы ввели неверное количество данных. Требуется ввести данные в следующем формате: Фамилия Имя Отчество дата_рождения номер_телефона пол.");
-            return;
         }
 
-        try {
-            validateAndProcessData(parts);
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
+        validateAndProcessData(parts);
     }
 
     private static void validateAndProcessData(String[] data) throws IOException {
@@ -54,9 +59,8 @@ public class UserDataProcessor {
 
     private static void writeToFIle(String[] data) throws IOException {
         String fileName = data[0] + ".txt";
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName, true))) {
-            writer.write(String.join(" ", data));
-            writer.newLine();
+        try (OutputStreamWriter writer = new OutputStreamWriter(new FileOutputStream(fileName, true), "UTF-8")) {
+            writer.write(String.join(" ", data) + "\n");
         }
     }
 }
